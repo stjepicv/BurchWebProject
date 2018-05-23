945e4a4aa2ca917b74e64198138c00fc982fa0bb
stjepicv <valentin.stjepiccosic.2012@gmail.com>
2018-05-24 01:30:07 +0200

    Add images to items in database

--- api/models/Item.class.php
+++ api/models/Item.class.php
@@ -8,6 +8,20 @@
 
         public function getAll() {
             $sql = 'SELECT * FROM item';
-            return $this->pdo->query($sql)->fetchAll();
+            $items = $this->pdo->query($sql)->fetchAll();
+            
+            for($i = 0; $i < count($items); $i++) {
+                $items[$i]['images'] = $this->getImages($items[$i]['id']);
+            }
+
+            return $items;
+        }
+
+        private function getImages($itemId) {
+            $sql = 'SELECT filename FROM item_image WHERE item_id = :item_id';
+            $statement = $this->pdo->prepare($sql);
+            $statement->execute(['item_id' => $itemId]);
+            
+            return $statement->fetchAll();
         }
     }
--- database.sql
+++ database.sql
@@ -13,8 +13,10 @@ CREATE TABLE item_image(
     filename VARCHAR(255),
 
     PRIMARY KEY(id),
-    FOREIGN KEY(item_id) REFERENCES item(id)
+    FOREIGN KEY(item_id) REFERENCES item(id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
 
 INSERT INTO item(name, description, price) VALUES('bread', 'white bread', 1.0);
+SET @item_id = LAST_INSERT_ID();
+INSERT INTO item_image(item_id, filename) VALUES(@item_id, '350x350.png');
