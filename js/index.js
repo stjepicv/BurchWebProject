0e929d89085dad487cf4d6ad325278892bb64735
stjepicv <valentin.stjepiccosic.2012@gmail.com>
2018-05-27 01:22:25 +0200

    Add login functionality

--- js/index.js
+++ js/index.js
@@ -14,4 +14,25 @@ $(document).ready(function () {
     })
 
     app.run()
+
+
+
+    $('#button-login').on('click', submitLogin)
 })
+
+
+var loginData = null
+
+function submitLogin(event) {
+    $.post({
+        url: apiUrl + '/user/login',
+        data: $('#login-form').serialize(),
+        success: function(response) {
+            loginData = response
+            $('#login-modal').modal('hide')
+        },
+        error: function(error) {
+            console.log(error)
+        }
+    })
+}
