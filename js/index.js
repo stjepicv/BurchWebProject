b82866e721c14f18daedc23e35b1f38a716b93e9
stjepicv <valentin.stjepiccosic.2012@gmail.com>
2018-05-28 13:41:07 +0200

    Move cart to items.html:

--- js/index.js
+++ js/index.js
@@ -18,10 +18,6 @@ $(document).ready(function () {
 
 
     $('#button-login').on('click', submitLogin)
-    $('#button-cart-clear').on('click', clearCart)
-    $('#button-cart-order').on('click', makeOrder)
-
-    
     $('#button-show-login').on('click', function() {
         if(loginData == null) {
             $('#login-modal').modal('show')
@@ -58,56 +54,6 @@ function submitLogin(event) {
 }
 
 
-/* cart */
-var cartItems = []
-
-function addToCart(item) {
-    cartItems.push(item)
-    reloadCartHtml()
-}
-
-function clearCart() {
-    cartItems = []
-    reloadCartHtml()
-}
-
-function reloadCartHtml() {
-    $('#list-cart').empty()
-    $.each(cartItems, function(index, item) {
-        var par = $('<li>' + item.name + '</li>')
-        $('#list-cart').append(par)
-    })
-}
-
-function makeOrder() {
-    if(cartItems.length == 0) {
-        showMessage('Put some items in your cart first')
-    }
-    else if(loginData == null) {
-        showMessage('You have to log in first')
-    }
-    else {
-        showQuestion('Are you sure you want to make this order?', function(answer) {
-            if(answer === true) {
-                $.post({
-                    url: apiUrl + '/order/create',
-                    data: {
-                        'token': loginData.token,
-                        'itemIds': $.map(cartItems, function(item) {
-                            return item.id
-                        })
-                    },
-                    success: function(response) {
-                        showMessage('Order created successfully')
-                    },
-                    error: function(error) {
-                        showMessage('There was an error while creating the order')
-                    }
-                })
-            }
-        })
-    }
-}
 
 
 
--- js/register.js
+++ js/register.js
@@ -1,3 +1,4 @@
+
 $('#register-form').on('submit', function(e) {
     e.preventDefault()
 
@@ -5,11 +6,35 @@ $('#register-form').on('submit', function(e) {
         url: apiUrl + '/user/register',
         data: $(this).serialize(),
         success: function(response) {
+            showMessage('You have registered successfully')
         },
         error: function(error) {
+            showMessage('Something went wrong')
+            console.log(error)
         }
     })
 })
-
+/*
 $('#register-form').validate({
+    rules: {
+        email: {
+            email: true
+        },
+        username: {
+            minlength: 5,
+            maxlenght: 30
+        },
+        password: {
+            minlength: 6,
+            maxlenght: 50
+        },
+        password2: {
+            minlength: 6,
+            maxlenght: 50
+        }
+    },
+    submitHandler: function(form) {
+        console.log(form)
+    }
 })
+*/
